d1bc39e26f8d5d95cf163e6d607a0ac0612741e6
kep0fff <maksvevgexi@gmail.com>
2024-10-07 20:44:44 +0300

    PopUp

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 
-function Categories({ items, onClick }) {
+function Categories({ items }) {
 	const [activeItem, setActiveItem] = React.useState(null);
-	const onSelectItem = (index) => {
+	const onSelectItem = index => {
 		setActiveItem(index);
-	}
+	};
 	return (
 		<div className='categories'>
 			<ul>
 				<li className={activeItem === null ? 'active' : ''} onClick={() => setActiveItem(null)}>
 					Все
 				</li>
-				{items && items.map((name, index) => (
-					<li
-						className={activeItem === index ? 'active' : ''}
-						onClick={() => {
-							onSelectItem(index);
-						}}
-						key={`${name}_${index}`}>
-						{name}
-					</li>
-				))}
+				{items &&
+					items.map((name, index) => (
+						<li
+							className={activeItem === index ? 'active' : ''}
+							onClick={() => {
+								onSelectItem(index);
+							}}
+							key={`${name}_${index}`}>
+							{name}
+						</li>
+					))}
 			</ul>
 		</div>
 	);
