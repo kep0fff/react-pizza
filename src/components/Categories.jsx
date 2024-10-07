9a6a7ab291b5240fbdfe5a8ea3780b50633ea281
kep0fff <maksvevgexi@gmail.com>
2024-10-07 19:08:39 +0300

    Categories Function

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
-function Categories({ items }) {
+function Categories({ items, onClick }) {
+	const [activeItem, setActiveItem] = React.useState(null);
+	const onSelectItem = (index) => {
+		setActiveItem(index);
+	}
 	return (
 		<div className='categories'>
 			<ul>
-				<li className='active'>Все</li>
-				{items.map(name => (
-					<li>{name}</li>
+				<li className={activeItem === null ? 'active' : ''} onClick={() => setActiveItem(null)}>
+					Все
+				</li>
+				{items && items.map((name, index) => (
+					<li
+						className={activeItem === index ? 'active' : ''}
+						onClick={() => {
+							onSelectItem(index);
+						}}
+						key={`${name}_${index}`}>
+						{name}
+					</li>
 				))}
 			</ul>
 		</div>
