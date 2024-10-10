2f98171496bb1bc02360d1fd303a171bca3add19
kep0fff <maksvevgexi@gmail.com>
2024-10-10 14:59:18 +0300

    PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React from 'react';
 import { Header } from './components';
 import { Home, Cart } from './pages';
-import { Routes, Route,} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
+	const [pizzas, setPizzas] = React.useState([]);
+	React.useEffect(() => {
+		fetch('http://localhost:3000/db.json')
+			.then(resp => resp.json())
+			.then(json => {
+				setPizzas(json.pizzas);
+			});
+	}, []);
 	return (
 		<div className='wrapper'>
 			<Header />
 			<div className='content'>
 				<Routes>
-					<Route path='/' element={<Home />} />
+					<Route path='/' element={<Home items = {pizzas} />} />
 					<Route path='/cart' element={<Cart />} />
 				</Routes>
 			</div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { } from 'react-router-dom';
+import {} from 'react-router-dom';
 
 function Cart() {
 	return (
 		<div>
-			<h1>132</h1>
+			<h1>52</h1>
 		</div>
 	);
 }
